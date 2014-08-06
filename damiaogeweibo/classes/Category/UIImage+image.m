2bc985c8c11ea0a8bc909e0d3af070dada8e020f
Singer <singer1026@gmail.com>
2014-08-06 23:53:12 +0800

    修正

--- damiaogeweibo/classes/Category/UIImage+image.m
+++ damiaogeweibo/classes/Category/UIImage+image.m
@@ -12,14 +12,6 @@
 +(UIImage *) fullscreenImageWithName:(NSString *)name{
     
     if (iPhone5) {
-        //        // 1.获取没有拓展名的文件名
-        //        NSString *filename = [name stringByDeletingPathExtension];
-        //
-        //        // 2.拼接-568h@2x
-        //        filename = [filename stringByAppendingString:@"-568h@2x"];
-        //
-        //        // 3.拼接拓展名
-        //        NSString *extension = [name pathExtension];
         name = [name filenameAppend:@"-568h@2x"];
     }
     return [UIImage imageNamed:name];
